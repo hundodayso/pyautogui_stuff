92218e15ebeeed9275090c060c74b89d1c98d984
hundodayso <solder-elision.0y@icloud.com>
2024-04-07 14:36:20 +0100

    1

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import cv2
 game_on = True
 for i in range(0,1):
     time.sleep(1)
-    im = pyautogui.screenshot(region=(855, 145, 1033, 1264))
+    im = pyautogui.screenshot(region=(785, 145, 1033, 1264))
     im.save(f'screenshot{i}.png')
     im_filename = (f'screenshot{i}.png')
     cv2_img = cv2.imread(im_filename,1)
@@ -16,3 +16,16 @@ for i in range(0,1):
     print(cv2_img[10][1000])
 
     pyautogui.click(104,130)
+
+
+
+####some notes:
+###Portrait###
+#Top Left: x785, y=145
+#Bottom Right: x1759, y=1404
+#Difference: x=974, y=1260
+
+####LANDSCAPE####
+#Top Left: x=642, y=145
+#Btoom Right: x=1902, y=1118
+#Difference: x=1260, y=974
